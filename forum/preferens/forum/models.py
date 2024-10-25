56fabf891fc33693154e5c3af8e75b20ed4223c5
moth <motyl9040@gmail.com>
2024-10-25 10:13:23 +0300

    изменен способ хранения файлов

--- forum/preferens/forum/models.py
+++ forum/preferens/forum/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Posts (models.Model):
     title = models.CharField(max_length=256)
     text_post = models.TextField()
-    image = models.ImageField(blank=True, upload_to='photos_buy_to_sell') #/%Y/%m
+    #models.ImageField(blank=True, upload_to='photos_forum') #/%Y/%m
     votes = models.IntegerField(default=0)
     data_published = models.DateTimeField(auto_now_add=True)
     author = models.CharField(max_length=100)
@@ -15,6 +15,21 @@ class Posts (models.Model):
         else:
             elipsis = ''
         return self.title[:20] + elipsis
+
+class Whitelist(models.Model):
+    files_format = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.files_format
+
+class Pars_files(models.Model):
+    key_posts = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    file = models.FileField(blank=True, upload_to='photos_forum')
+    format = models.CharField(max_length=10)
+    hash_name = models.TextField()
+
+    def __str__(self):
+        return str(self.file)
 
 class Array_Like(models.Model):
     id_post = models.IntegerField()
--- forum/preferens/forum/views.py
+++ forum/preferens/forum/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Posts, Array_Like
+from .models import Posts, Array_Like, Whitelist, Pars_files
 from django.db.models import F
 from django.urls import reverse
+from django.http import HttpResponse
+import re
+import hashlib
 
 
 def home(request):
@@ -13,9 +16,15 @@ def home(request):
 
 def post_inp(request, post_id):
     like(request, post_id)
+    request_db = Pars_files.objects.filter(key_posts_id=post_id)[0]
+    file = request_db.file
+    print(file.name)
+
+
     data = Posts.objects.get(id=post_id)
     context = {
-        "data":data
+        "data":data,
+        'file':file
     }
     return render(request, "forum/post.html", context)
 
@@ -34,8 +43,6 @@ def time_pub (request):
     return render(request, "forum/time_pub_t.html", context)
 
 def search_pub (request):
-
-    print(request.POST)
     if request.POST:
         search_resalt = Posts.objects.filter(title=request.POST['request_from_template'])
         response_length = len(search_resalt)
@@ -48,16 +55,35 @@ def search_pub (request):
     }
     return render(request, "forum/search_t.html", context)
 
-def create_post(request):
+def create_post(request):# TO DO сделать прием post запр файла на pars_files
     if not request.user.is_authenticated:
         return redirect(reverse('login'), permanent=True)
     if request.method == "POST":
-        Posts(title=request.POST['title'], text_post=request.POST['text_post'], author=request.user,
-              image=request.FILES['image']).save()
+        Posts(title=request.POST['title'], text_post=request.POST['text_post'], author=request.user,).save()
+        id_post = Posts.objects.order_by('-id')[0].id # мне это не нравится
+        print(id_post)
+        if request.FILES['image']:
+            name = str(re.findall('.{1,100}[.]', request.FILES['image'].name)[0][:-1])
+            format_files = str(re.findall('[.]\w{3,5}\\b', str(request.FILES["image"]))[-1]).upper()
+            file = request.FILES['image']
+            file.name = name
+            print(file)
+            print(type(file.name))
+            hash_name = hashlib.md5(name.encode()).hexdigest()
+            print(type(name))
+            print(hash_name)
+            print(Whitelist.objects.get(files_format=format_files))
+            if Whitelist.objects.get(files_format=format_files):
+                Pars_files(file=file, format=format_files, hash_name=hash_name, key_posts_id=id_post).save()
+            else:
+                return HttpResponse('<h3>Недопустимый формат файла</h3>')
+
+
     return render(request, 'forum/create_post.html')
 
+#сделать парс формата с пом вайт листа и вайт лист распределить по файлам наприм изобр
+
 def like(request, id_objetct):
-    print(request.POST)
     if request.user.is_authenticated:
         if request.POST:
             id_user = request.user.id
